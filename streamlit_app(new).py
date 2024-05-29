85fe34654a1ec6a6a2afff9aefe3c6322d26e0ec
Fransisca Mulya Sari <128776177+FransiscaaMS@users.noreply.github.com>
2024-05-29 16:37:34 +0700

    Update streamlit_app(new).py

--- streamlit_app(new).py
+++ streamlit_app(new).py
@@ -2,8 +2,7 @@ import pickle
 import streamlit as st
 import pandas as pd
 
-
-path_to_file = "C:\\Users\\Fransisca Mulya Sari\\Documents\\GitHub\\Large_file"
+path_to_file = "Large_file/prediksi_harga_rumah_smg.sav"  
 model = pickle.load(open(path_to_file, 'rb'))
 
 # Membuat sidebar
